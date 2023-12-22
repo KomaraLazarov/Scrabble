ee357e9b5861a4be1522fc2d00d0f36f8606083d
Никола Лазаров <nl75102679@edu.mon.bg>
2023-12-22 18:59:36 +0200

    Create the function about random generation of letters and their printing

--- Scrabble/Scrabble.cpp
+++ Scrabble/Scrabble.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
 const char FILE_NAME[] = "WordsDictionary.txt";
+const unsigned WORD_MAX_LENGTH = 100;
+const unsigned ENGLISH_LETTERS_COUNT = 26;
+
+void printGeneratedLetters(const char* letters, unsigned lettersCount)
+{
+	for (size_t i = 0; i < lettersCount; i++)
+	{
+		cout << letters[i] << " ";
+	}
+	cout << endl;
+}
+
+void generateRandomLetters(char* letters, unsigned lettersCount)
+{
+	for (size_t i = 0; i < lettersCount; i++)
+	{
+		int number = rand() % ENGLISH_LETTERS_COUNT;
+		letters[i] = number + 'a';
+	}
+	letters[lettersCount] = '\0';
+
+	printGeneratedLetters(letters, lettersCount);
+}
 
 int readFile(const char filename[], char**& words, unsigned& wordsCount)
 {
@@ -31,8 +55,12 @@ int readFile(const char filename[], char**& words, unsigned& wordsCount)
 
 int main()
 {
+	srand(time(NULL));
 	char** words = nullptr;
 	unsigned wordsCount = 0;
+
+	char* arr = new char[11];
+	generateRandomLetters(arr, 10);
 
 	readFile(FILE_NAME, words, wordsCount);
 }
